0de8aad01701514c33595d12142f759509346fb6
Chris Zuber <shgysk8zer0@gmail.com>
2015-06-02 14:21:53 -0700

    Updates to updatready listener script
    An attempt at getting this to work correctly. Something wrong still,
    though the JavaScript seems fine (it all works in Chrome)

--- appcache_listener.js
+++ appcache_listener.js
@@ -1,14 +1,10 @@
-if ('applicationCache' in window && document.documentElement.hasAttribute('manifest')) {
-	window.addEventListener('load', function()
-	{
-		applicationCache.addEventListener('updateready', function (e)
-		{
-			if (applicationCache.status === applicationCache.UPDATEREADY) {
-				applicationCache.update() && applicationCache.swapCache();
-				if (confirm('A new version of this site is available. Load it?')) {
-					window.location.reload();
-				}
+if (('applicationCache' in window) && document.documentElement.hasAttribute('manifest')) {
+	applicationCache.addEventListener('updateready', function(event) {
+		if (applicationCache.status === applicationCache.UPDATEREADY) {
+			applicationCache.update() && applicationCache.swapCache();
+			if (confirm('A new version of this site is available. Load it?')) {
+				location.reload();
 			}
-		});
+		}
 	});
 }
