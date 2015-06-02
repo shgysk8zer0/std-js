4515819b6cee1e1970dcedf1dd0fb354b6ef38e0
Chris Zuber <shgysk8zer0@gmail.com>
2015-06-01 21:14:11 -0700

    Add script to add support for HTMLImports
    Script checks or import support on link elements and adds where not
    supported.

---
+++ polyfills/htmlimport.js
@@ -0,0 +1,14 @@
+if (! ('import' in HTMLLinkElement.prototype)) {
+	Object.defineProperty(HTMLLinkElement.prototype, 'import', {
+		get: function() {
+			var xhr = new XMLHttpRequest();
+			xhr.open(
+				'GET',
+				this.href,
+				false
+			);
+			xhr.send();
+			return new DOMParser().parseFromString(xhr.response, "text/html");
+		}
+	});
+}
