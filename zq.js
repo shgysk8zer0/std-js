c86c811c76ad00a357e8321bff7a8bea0bebb5a5
Chris Zuber <shgysk8zer0@gmail.com>
2015-08-07 20:38:31 -0700

    Create Babel output of zq.es6

--- zq.js
+++ zq.js
@@ -1,282 +1,472 @@
-/*======================================================zQ Functions=========================================================*/
-function zQ(q) {
-	if(typeof q === 'undefined') {
-		q = document.documentElement;
-	}
-	this.query = q;
-	try {
-		switch(typeof this.query) {
-			case 'string':
-				this.results = document.querySelectorAll(this.query);
-				break;
+/*eslint no-use-before-define: 0*/
+/*============================ zQ Functions =======================*/
+'use strict';
 
-			default:
-				this.results = [this.query];
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var zQ = (function () {
+	function zQ(q) {
+		_classCallCheck(this, zQ);
+
+		if (typeof q === 'undefined') {
+			q = document.documentElement;
 		}
-	} catch (error) {
-		console.error(error, this);
-		console.error('No results for ' + this.query);
-	}
-	this.length = this.results.length;
-	this.found = (!!this.results.length);
-	this.filters = [];
-	return this;
-}
-Object.prototype.isZQ = false;
-zQ.prototype.isZQ = true;
-function $(q) {
-	if(typeof q === 'undefined') {
-		q = document.documentElement;
-	} else if(q.isZQ) {
-		return q;
-	}
-	return new zQ(q);
-}
-zQ.prototype.constructor = zQ;
-zQ.prototype.get = function(n) {
-	return this.results.item(n);
-};
-zQ.prototype.each = function(callback) {
-	if(this.found) {
-		this.results.forEach(callback);
-	}
-	return this;
-};
-zQ.prototype.toArray = function() {
-	if(!this.results.isArray) {
-		var temp = [];
-		this.each(function(node) {
-			temp.push(node);
-		});
-		this.results = temp;
-	}
-	return this;
-};
-zQ.prototype.indexOf = function(i) {
-	return this.results.indexOf(i);
-};
-zQ.prototype.some = function(callback) {
-	return this.results.some(callback);
-};
-zQ.prototype.every = function(callback) {
-	return this.results.every(callback);
-};
-zQ.prototype.filter = function(callback) {
-	this.filters.push(callback.toString());
-	this.results = this.results.filter(callback);
-	this.length = this.results.length;
-	return this;
-};
-zQ.prototype.map = function(callback) {
-	return this.results.map(callback);
-};
-zQ.prototype.addClass = function(cname) {
-	this.each(function(el) {
-		el.classList.add(cname);
-	});
-	return this;
-};
-zQ.prototype.removeClass = function(cname) {
-	this.each(function(el) {
-		el.classList.remove(cname);
-	});
-	return this;
-};
-zQ.prototype.hasClass = function(cname) {
-	return this.some(function(el) {
-		return el.classList.contains(cname);
-	});
-};
-zQ.prototype.toggleClass = function(cname, condition) {
-	if (typeof condition === 'undefined') {
-		this.each(function(el) {
-			el.classList.toggle(cname);
-		});
-	} else {
-		this.each(function(el) {
-			el.classList.toggle(cname, condition);
-		});
-	}
-	return this;
-};
-zQ.prototype.swapClass = function(cname1, cname2) {
-	this.each(function(el) {
-		el.classList.swap(cname1, cname2);
-	});
-	return this;
-};
-zQ.prototype.pickClass = function(cname1, cname2, condition) {
-	(condition) ? this.addClass(cname1) : this.addClass(cname2);
-	return this;
-};
-zQ.prototype.delete = function() {
-	this.each(function(el) {
-		el.parentElement.removeChild(el);
-	});
-};
-zQ.prototype.hasAttribute = function(attr) {
-	return this.some(function(el) {
-		return el.hasAttribute(attr);
-	});
-};
-zQ.prototype.attr = function(attr, val) {
-	if(typeof val == 'undefined' || val === true) {
-		val = '';
-	}
-	if(val === false) {
-		this.each(function(el) {
-			el.removeAttribute(attr);
-		});
-	} else {
-		this.each(function(el) {
-			el.setAttribute(attr, val);
-		});
-	}
-	return this;
+		this.query = q;
+		try {
+			switch (typeof this.query) {
+				case 'string':
+					this.results = document.querySelectorAll(this.query);
+					break;
 
-};
-zQ.prototype.pause = function() {
-	this.each(function(media) {
-		media.pause();
-	});
-	return this;
-};
-/*======================================================Listener Functions=========================================================*/
+				default:
+					this.results = [this.query];
+			}
+		} catch (error) {
+			console.error(error, this);
+			console.error('No results for ' + this.query);
+		}
+		this.length = this.results.length;
+		this.found = this.results.length !== 0;
+		this.filters = [];
+	}
 
-zQ.prototype.on = function (event, callback) {
-	this.each(function (e) {
-		('addEventListener' in Element.prototype) ? e.addEventListener(event, callback, true) : e['on' + event] = callback;
-	});
-	return this;
-};
-/*Listeners per event type*/
-[
-	'click',
-	'dblclick',
-	'contextmenu',
-	'keypress',
-	'keyup',
-	'keydown',
-	'mouseenter',
-	'mouseleave',
-	'mouseover',
-	'mouseout',
-	'mousemove',
-	'mousedown',
-	'mouseup',
-	'input',
-	'change',
-	'submit',
-	'reset',
-	'invalid',
-	'select',
-	'focus',
-	'blur',
-	'resize',
-	'updateready',
-	'DOMContentLoaded',
-	'load',
-	'unload',
-	'beforeunload',
-	'abort',
-	'error',
-	'scroll',
-	'drag',
-	'offline',
-	'online',
-	'visibilitychange',
-	'popstate',
-	'pagehide'
-].forEach(function(event) {
-	zQ.prototype[event] = function(callback) {
-		return this.on(event, callback);
-	};
-});
-zQ.prototype.ready = function(callback) {
-	return this.on('DOMContentLoaded', callback);
-};
-zQ.prototype.networkChange = function (callback) {
-	return this.online(callback) .offline(callback);
-};
-zQ.prototype.playing = function (callback) {
-	this.each(function (e) {
-		/*Does not work with listeners. Use onEvent by default*/
-		e.onplay = callback;
-	});
-	return this;
-};
-zQ.prototype.paused = function (callback) {
-	this.each(function (e) {
-		e.onpause = callback;
-	});
-	return this;
-};
-zQ.prototype.visibilitychange = function (callback) {
-	this.each(function (e) {
-		[
-			'',
-			'moz',
-			'webkit',
-			'ms'
-		].forEach(function (pre) {
-			$(e) .on(pre + 'visibilitychange', callback);
-		});
-	});
-	return this;
-};
-zQ.prototype.watch = function(watching, options, attributeFilter) {
-	/*https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver*/
-	if(typeof options === 'undefined') {
-		options = [];
-	}
-	var watcher = new MutationObserver(function(mutations) {
-		mutations.forEach(function(mutation) {
-			watching[mutation.type].call(mutation);
-		});
-	}),
-	watches = {};
-	Object.keys(watching).concat(options).forEach(function(event) {
-		watches[event] = true;
-	});
-	if(typeof attributeFilter !== 'undefined' && attributeFilter.isArray) {
-		watches.attributeFilter = attributeFilter;
-	}
-	this.each(function(el) {
-		watcher.observe(el, watches);
-	});
-	return this;
-};
-/*====================================================================================================================*/
-zQ.prototype.$ = function (selector) {
-	return $(this.query.split(',').map(function(str) {
-		return selector.split(',').map(function(q) {
-			return str.trim() + ' ' + q.trim();
-		});
-	}).join(', '));
-};
-Object.prototype.$ = function(q) {
-	if(this === document || this === window) {
+	_createClass(zQ, [{
+		key: 'item',
+		value: function item(n) {
+			return this.results.item(n);
+		}
+	}, {
+		key: 'each',
+		value: function each(callback) {
+			if (this.found) {
+				this.results.forEach(callback);
+			}
+			return this;
+		}
+	}, {
+		key: 'toArray',
+		value: function toArray() {
+			if (!this.results.isArray) {
+				this.results = Array.prototype.slice.call(this.results, 0);
+			}
+			return this;
+		}
+	}, {
+		key: 'indexOf',
+		value: function indexOf(i) {
+			return this.results.indexOf(i);
+		}
+	}, {
+		key: 'some',
+		value: function some(callback) {
+			return this.results.some(callback);
+		}
+	}, {
+		key: 'every',
+		value: function every(callback) {
+			return this.results.every(callback);
+		}
+	}, {
+		key: 'filter',
+		value: function filter(callback) {
+			this.filters.push(callback.toString());
+			this.results = this.results.filter(callback);
+			this.length = this.results.length;
+			return this;
+		}
+	}, {
+		key: 'map',
+		value: function map(callback) {
+			return this.results.map(callback);
+		}
+	}, {
+		key: 'addClass',
+		value: function addClass(cname) {
+			this.each(function (el) {
+				return el.classList.remove(cname);
+			});
+			return this;
+		}
+	}, {
+		key: 'removeClass',
+		value: function removeClass(cname) {
+			this.each(function (el) {
+				return el.classList.remove(cname);
+			});
+			return this;
+		}
+	}, {
+		key: 'hasClass',
+		value: function hasClass(cname) {
+			return this.some(function (el) {
+				return el.classList.contains(cname);
+			});
+		}
+	}, {
+		key: 'toggleClass',
+		value: function toggleClass(cname, condition) {
+			if (typeof condition === 'undefined') {
+				this.each(function (el) {
+					return el.classList.toggle(cname);
+				});
+			} else {
+				this.each(function (el) {
+					return el.classList.toggle(cname, condition);
+				});
+			}
+			return this;
+		}
+	}, {
+		key: 'swapClass',
+		value: function swapClass(cname1, cname2) {
+			this.each(function (el) {
+				return el.classList.swap(cname1, cname2);
+			});
+			return this;
+		}
+	}, {
+		key: 'pickClass',
+		value: function pickClass(cname1, cname2, condition) {
+			condition ? this.addClass(cname1) : this.addClass(cname2);
+			return this;
+		}
+	}, {
+		key: 'delete',
+		value: function _delete() {
+			this.each(function (el) {
+				return el.remove();
+			});
+		}
+	}, {
+		key: 'hasAttribute',
+		value: function hasAttribute(attr) {
+			return this.some(function (el) {
+				return el.hasAttribute(attr);
+			});
+		}
+	}, {
+		key: 'attr',
+		value: function attr(_attr, val) {
+			if (typeof val == 'undefined' || val === true) {
+				val = '';
+			}
+			if (val === false) {
+				this.each(function (el) {
+					return el.removeAttribute(_attr);
+				});
+			} else {
+				this.each(function (el) {
+					return el.setAttribute(_attr, val);
+				});
+			}
+			return this;
+		}
+	}, {
+		key: 'pause',
+		value: function pause() {
+			this.each(function (media) {
+				return media.pause();
+			});
+			return this;
+		}
+
+		/*==================== Listener Functions =================================*/
+	}, {
+		key: 'on',
+		value: function on(event, callback) {
+			this.each(function (e) {
+				'addEventListener' in Element.prototype ? e.addEventListener(event, callback, true) : e['on' + event] = callback;
+			});
+			return this;
+		}
+	}, {
+		key: 'ready',
+		value: function ready(callback) {
+			return this.on('DOMContentLoaded', callback);
+		}
+	}, {
+		key: 'networkChange',
+		value: function networkChange(callback) {
+			return this.online(callback).offline(callback);
+		}
+	}, {
+		key: 'playing',
+		value: function playing(callback) {
+			this.each(function (e) {
+				return e.onplay = callback;
+			});
+			return this;
+		}
+	}, {
+		key: 'paused',
+		value: function paused(callback) {
+			this.each(function (e) {
+				return e.onpause = callback;
+			});
+			return this;
+		}
+	}, {
+		key: 'visibilitychange',
+		value: function visibilitychange(callback) {
+			this.each(function (e) {
+				['', 'moz', 'webkit', 'ms'].forEach(function (pre) {
+					$(e).on(pre + 'visibilitychange', callback);
+				});
+			});
+			return this;
+		}
+	}, {
+		key: 'click',
+		value: function click(callback) {
+			return this.on('click', callback);
+		}
+	}, {
+		key: 'dblclick',
+		value: function dblclick(callback) {
+			this.on('dblclick', callback);
+		}
+	}, {
+		key: 'contextmenu',
+		value: function contextmenu(callback) {
+			return this.on('contextmenu', callback);
+		}
+	}, {
+		key: 'keypress',
+		value: function keypress(callback) {
+			return this.on('keypress', callback);
+		}
+	}, {
+		key: 'keyup',
+		value: function keyup(callback) {
+			return this.on('keyup', callback);
+		}
+	}, {
+		key: 'keydown',
+		value: function keydown(callback) {
+			return this.on('keydown', callback);
+		}
+	}, {
+		key: 'mouseenter',
+		value: function mouseenter(callback) {
+			return this.on('mouseenter', callback);
+		}
+	}, {
+		key: 'mouseleave',
+		value: function mouseleave(callback) {
+			return this.on('mouseleave', callback);
+		}
+	}, {
+		key: 'mouseover',
+		value: function mouseover(callback) {
+			return this.on('mouseover', callback);
+		}
+	}, {
+		key: 'mouseout',
+		value: function mouseout(callback) {
+			return this.on('mouseout', callback);
+		}
+	}, {
+		key: 'mousemove',
+		value: function mousemove(callback) {
+			return this.on('mousemove', callback);
+		}
+	}, {
+		key: 'mousedown',
+		value: function mousedown(callback) {
+			return this.on('mousedown', callback);
+		}
+	}, {
+		key: 'mouseup',
+		value: function mouseup(callback) {
+			return this.on('mouseup', callback);
+		}
+	}, {
+		key: 'input',
+		value: function input(callback) {
+			return this.on('input', callback);
+		}
+	}, {
+		key: 'change',
+		value: function change(callback) {
+			return this.on('change', callback);
+		}
+	}, {
+		key: 'submit',
+		value: function submit(callback) {
+			return this.on('submit', callback);
+		}
+	}, {
+		key: 'reset',
+		value: function reset(callback) {
+			return this.on('reset', callback);
+		}
+	}, {
+		key: 'invalid',
+		value: function invalid(callback) {
+			return this.on('invalid', callback);
+		}
+	}, {
+		key: 'select',
+		value: function select(callback) {
+			return this.on('select', callback);
+		}
+	}, {
+		key: 'focus',
+		value: function focus(callback) {
+			return this.on('focus', callback);
+		}
+	}, {
+		key: 'blur',
+		value: function blur(callback) {
+			return this.on('blur', callback);
+		}
+	}, {
+		key: 'resize',
+		value: function resize(callback) {
+			return this.on('resize', callback);
+		}
+	}, {
+		key: 'updateready',
+		value: function updateready(callback) {
+			return this.on('updateready', callback);
+		}
+	}, {
+		key: 'DOMContentLoaded',
+		value: function DOMContentLoaded(callback) {
+			return this.on('DOMContentLoaded', callback);
+		}
+	}, {
+		key: 'load',
+		value: function load(callback) {
+			return this.on('load', callback);
+		}
+	}, {
+		key: 'unload',
+		value: function unload(callback) {
+			return this.on('unload', callback);
+		}
+	}, {
+		key: 'beforeunload',
+		value: function beforeunload(callback) {
+			return this.on('beforeunload', callback);
+		}
+	}, {
+		key: 'abort',
+		value: function abort(callback) {
+			return this.on('abort', callback);
+		}
+	}, {
+		key: 'error',
+		value: function error(callback) {
+			return this.on('error', callback);
+		}
+	}, {
+		key: 'scroll',
+		value: function scroll(callback) {
+			return this.on('scroll', callback);
+		}
+	}, {
+		key: 'drag',
+		value: function drag(callback) {
+			return this.on('drag', callback);
+		}
+	}, {
+		key: 'offline',
+		value: function offline(callback) {
+			return this.on('offline', callback);
+		}
+	}, {
+		key: 'online',
+		value: function online(callback) {
+			return this.on('online', callback);
+		}
+
+		/*visibilitychange(callback) {
+  	return this.on('visibilitychange', callback);
+  }*/
+	}, {
+		key: 'popstate',
+		value: function popstate(callback) {
+			return this.on('popstate', callback);
+		}
+	}, {
+		key: 'pagehide',
+		value: function pagehide(callback) {
+			return this.on('pagehide', callback);
+		}
+	}, {
+		key: 'watch',
+		value: function watch(watching, options, attributeFilter) {
+			/*https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver*/
+			if (typeof options === 'undefined') {
+				options = [];
+			}
+			var watcher = new MutationObserver(function (mutations) {
+				mutations.forEach(function (mutation) {
+					watching[mutation.type].call(mutation);
+				});
+			}),
+			    watches = {};
+			Object.keys(watching).concat(options).forEach(function (event) {
+				watches[event] = true;
+			});
+			if (typeof attributeFilter !== 'undefined' && attributeFilter.isArray) {
+				watches.attributeFilter = attributeFilter;
+			}
+			this.each(function (el) {
+				watcher.observe(el, watches);
+			});
+			return this;
+		}
+
+		/*====================================================================================================================*/
+	}, {
+		key: '$',
+		value: (function (_$) {
+			function $(_x) {
+				return _$.apply(this, arguments);
+			}
+
+			$.toString = function () {
+				return _$.toString();
+			};
+
+			return $;
+		})(function (selector) {
+			return $(this.query.split(',').map(function (str) {
+				return selector.split(',').map(function (q) {
+					return str.trim() + ' ' + q.trim();
+				});
+			})).join(', ');
+		})
+	}, {
+		key: 'css',
+		value: function css(args) {
+			var style = document.styleSheets[document.styleSheets.length - 1];
+			style.insertRule(this.query + ' {' + args + '}', style.cssRules.length);
+			return this;
+		}
+	}]);
+
+	return zQ;
+})();
+
+Object.prototype.$ = function (q) {
+	if (this === document || this === window) {
 		return $(q);
 	}
 	return $(this).$(q);
 };
-zQ.prototype.css = function (args) { /*Set style using CSS syntax*/
-	/*var n,
-		i,
-		e,
-		value = [
-		];
-	args = args.replace('; ', ';') .replace(': ', ':') .replace('float', 'cssFloat') .split(';');
-	for (var i = 0; i < args.length; i++) {
-		value[i] = args[i].slice(args[i].indexOf(':') + 1).trim();
-		args[i] = args[i].slice(0, args[i].indexOf(':')).trim().camelCase();
+Object.prototype.isZQ = false;
+zQ.prototype.isZQ = true;
+function $(q) {
+	if (typeof q === 'undefined') {
+		q = document.documentElement;
+	} else if (q.isZQ) {
+		return q;
 	}
-	for (var i = 0; i < args.length; i++) {
-		this.each(function (e) {
-			e.style[args[i]] = value[i];
-		});
-	}*/
-	var style = document.styleSheets[document.styleSheets.length - 1];
-	style.insertRule(this.query + '{' + args + '}', style.cssRules.length);
-	return this;
-};
+	return new zQ(q);
+}
+
