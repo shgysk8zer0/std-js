57781733dcd1294bdf77bd0e51225b395d3f3482
Chris Zuber <shgysk8zer0@gmail.com>
2017-05-24 15:04:22 -0700

    Fix queries returning generators
    Use `await fetch` directly in static methods

--- keybase.js
+++ keybase.js
@@ -1,74 +1,125 @@
 const ENDPOINT = 'https://keybase.io';
 const VERSION = '1.0';
 
-async function get(url) {
-	console.info(url);
-	try {
-		const resp = await fetch(url, {
-			mode: 'cors'
-		});
-		if (resp.ok) {
-			const type = resp.headers.get('Content-Type');
-			if (type.includes('application/json')) {
-				let json = await resp.json();
-				console.log(json);
-				return json;
-			} else if (type.includes('text/html')) {
-				let html = await resp.html();
-				let parser = new DOMParser();
-				return parser.parse(html, 'text/html');
-			} else {
-				return resp.text();
-			}
-		} else {
-			throw new Error(`<"${resp.url}"> [${resp.status}:${resp.statusText}]`);
-		}
-	} catch(e) {
-		console.error(e);
-	}
-}
-
 export default class KeyBase {
 	static async getKey(user) {
 		const url = new URL(`/${user}/pgp_keys.asc`, ENDPOINT);
-		let resp = await get(url);
-		return resp;
+		try {
+			const resp = await fetch(url, {mode: 'cors'});
+			if (resp.ok) {
+				const json = await resp.json();
+				return json;
+			} else {
+				throw new Error(`<${resp.url}> [${resp.status} ${resp.statusText}]`);
+			}
+		} catch(err) {
+			console.error(err);
+		}
 	}
 
 	static async searchUsers(...users) {
 		const url = new URL(`/_/api/${VERSION}/user/lookup.json`, ENDPOINT);
 		url.searchParams.set('usernames', users);
-		let resp = await get(url);
-		return resp;
+		try {
+			const resp = await fetch(url, {mode: 'cors'});
+			if (resp.ok) {
+				const json = await resp.json();
+				return json;
+			} else {
+				throw new Error(`<${resp.url}> [${resp.status} ${resp.statusText}]`);
+			}
+		} catch(err) {
+			console.error(err);
+		}
 	}
 	static async searchTwitter(user) {
 		const url = new URL(`/_/api/${VERSION}/user/lookup.json`, ENDPOINT);
 		url.searchParams.set('twitter', user);
-		return get(url);
+		try {
+			const resp = await fetch(url, {mode: 'cors'});
+			if (resp.ok) {
+				const json = await resp.json();
+				return json;
+			} else {
+				throw new Error(`<${resp.url}> [${resp.status} ${resp.statusText}]`);
+			}
+		} catch(err) {
+			console.error(err);
+		}
 	}
 	static async searchReddit(user) {
 		const url = new URL(`/_/api/${VERSION}/user/lookup.json`, ENDPOINT);
 		url.searchParams.set('reddit', user);
-		return get(url);
+		try {
+			const resp = await fetch(url, {mode: 'cors'});
+			if (resp.ok) {
+				const json = await resp.json();
+				return json;
+			} else {
+				throw new Error(`<${resp.url}> [${resp.status} ${resp.statusText}]`);
+			}
+		} catch(err) {
+			console.error(err);
+		}
 	}
 	static async searchHackerNews(user) {
 		const url = new URL(`/_/api/${VERSION}/user/lookup.json`, ENDPOINT);
 		url.searchParams.set('hackernews', user);
-		return get(url);
+		try {
+			const resp = await fetch(url, {mode: 'cors'});
+			if (resp.ok) {
+				const json = await resp.json();
+				return json;
+			} else {
+				throw new Error(`<${resp.url}> [${resp.status} ${resp.statusText}]`);
+			}
+		} catch(err) {
+			console.error(err);
+		}
 	}
 	static async searchGithub(user) {
 		const url = new URL(`/_/api/${VERSION}/user/lookup.json`, ENDPOINT);
 		url.searchParams.set('github', user);
-		return get(url);
+		try {
+			const resp = await fetch(url, {mode: 'cors'});
+			if (resp.ok) {
+				const json = await resp.json();
+				return json;
+			} else {
+				throw new Error(`<${resp.url}> [${resp.status} ${resp.statusText}]`);
+			}
+		} catch(err) {
+			console.error(err);
+		}
 	}
 	static async searchKey(key) {
 		const url = new URL(`/_/api/${VERSION}/user/lookup.json`, ENDPOINT);
 		url.searchParams.set('key_fingerprint', key);
-		return get(url);
+		try {
+			const resp = await fetch(url, {mode: 'cors'});
+			if (resp.ok) {
+				const json = await resp.json();
+				return json;
+			} else {
+				throw new Error(`<${resp.url}> [${resp.status} ${resp.statusText}]`);
+			}
+		} catch(err) {
+			console.error(err);
+		}
 	}
 	static async searchDomain(domain) {
 		const url = new URL(`/_/api/${VERSION}/user/lookup.json`, ENDPOINT);
 		url.searchParams.set('domain', domain);
-		return get(url);
+		try {
+			const resp = await fetch(url, {mode: 'cors'});
+			if (resp.ok) {
+				const json = await resp.json();
+				return json;
+			} else {
+				throw new Error(`<${resp.url}> [${resp.status} ${resp.statusText}]`);
+			}
+		} catch(err) {
+			console.error(err);
+		}
 	}
 }
