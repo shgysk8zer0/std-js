5d3287f8658a4d3d9da9a30e44c93228144f7f11
Chris Zuber <shgysk8zer0@gmail.com>
2017-07-17 09:49:49 -0700

    Use $.ready again
    `DOMContentLoaded` does not work unless `document.readyState` is still
    "loading"

--- test/index.js
+++ test/index.js
@@ -1,7 +1,8 @@
 import './shims.js';
 import {loadHandler} from './funcs.js';
 import kbdShortcuts from '../kbd_shortcuts.js';
+import {$} from '../functions.js';
 
 document.body.classList.replace('no-js', 'js');
-self.addEventListener('keypress', kbdShortcuts);
-self.addEventListener('load', loadHandler, {once: true});
+
+$(document).ready(loadHandler).keypress(kbdShortcuts);
