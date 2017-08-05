145cfda37d6ac6ca2f25160e03c8a97ef1cb342f
Chris Zuber <shgysk8zer0@gmail.com>
2017-08-05 08:12:57 -0700

    Add srcset & static getURL method to Gravatar
    - Any arguments after size are taken as a sizes array, and converted
    into `srcset`
    - Add static method to get Gravatar image URLs

--- Gravatar.js
+++ Gravatar.js
@@ -1,22 +1,36 @@
 import md5 from './md5.js';
+const domain = 'https://gravatar.com';
+
 export default class Gravatar extends Image {
-	constructor(email, size = 80) {
+	constructor(email, size, ...sizes) {
+		const url = Gravatar.getURL(email, size);
 		super(size);
-		const url = new URL(`/avatar/${md5(email)}`, 'https://gravatar.com/');
-		if (size !== 80) {
-			url.searchParams.set('s', size);
-		}
 		this.src = url;
 		this.alt = 'Gravatar';
+
+		if (sizes.length !== 0) {
+			this.srcset = sizes.map(size => {
+				url.searchParams.set('s', size);
+				return `${url} ${size}w`;
+			}).join(', ');
+			delete this.width;
+			delete this.height;
+		}
+	}
+
+	toString() {
+		return this.src;
 	}
 
 	set size(size) {
 		const url = new URL(this.src);
+
 		if (size === 80) {
 			url.searchParams.delete('s');
 		} else {
 			url.searchParams.set('s', size);
 		}
+
 		this.src = url;
 		this.width = size;
 		this.height = size;
@@ -27,7 +41,11 @@ export default class Gravatar extends Image {
 		return url.searchParams.has('s') ? parseInt(url.searchParams.get('s')) : 80;
 	}
 
-	toString() {
-		return this.src;
+	static getURL(email, size = null) {
+		const url = new URL(`/avatar/${md5(email)}`, domain);
+		if (Number.isInteger(size) && size !== 80) {
+			url.searchParams.set('s', size);
+		}
+		return url;
 	}
 }
