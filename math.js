58a3441f48c6b3f9416afd538d5c58e9845d293c
Chris Zuber <shgysk8zer0@gmail.com>
2017-11-08 00:32:58 -0800

    Add math shim and useful functions

---
+++ math.js
@@ -0,0 +1,53 @@
+import './math-shims.js';
+
+export function* range(start, end, step = 1) {
+	[start, end] = [Math.min(start, end), Math.max(start, end)];
+
+	while (start <= end) {
+		yield start;
+		start += step;
+	}
+}
+
+export function* fibonacci() {
+	let current = 1, prev = 1;
+	yield 1;
+
+	while (Number.isSafeInteger(current)) {
+		yield current;
+		[prev, current] = [current, current + prev];
+	}
+}
+
+export function randomFloat(min, max) {
+	[min, max] = [Math.min(min, max), Math.max(min, max)];
+	return Math.random() * (max - min + 1) + min;
+}
+
+export function randomInt(min, max) {
+	return Math.trunc(randomFloat(min, max));
+}
+
+export function sigma(start, end, callback) {
+	let sum = 0;
+	for (let num of range(start, end)) {
+		sum += callback(num);
+	}
+	return sum;
+}
+
+export function sum(...nums) {
+	return nums.reduce((sum, num) => sum + num, 0);
+}
+
+export function factorial(n) {
+	if (! Number.isInteger(n)) {
+		throw new TypeError(`Expected an Integer but got ${typeof n}`);
+	} else if (n > 1) {
+		return n * factorial(n - 1);
+	} else if (n === 1 || n === 0) {
+		return 1;
+	} else {
+		throw new TypeError(`Cannot handle numbers < 0. Got ${n}`);
+	}
+}
