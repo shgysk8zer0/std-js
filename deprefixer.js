8f08a78ed79b1c1bb074c89e068c9c267cf822f2
Chris Zuber <shgysk8zer0@gmail.com>
2017-12-28 11:18:23 -0800

    Deprefixer executes instead of exports

--- deprefixer.js
+++ deprefixer.js
@@ -1,26 +1,25 @@
-export default function() {
-	if (!('Notification' in window)) {
-		window.Notification = window.notifications || window.webkitNotifications || window.oNotifications || window.msNotifications || false;
-	}
-	if (!('indexedDB' in window)) {
-		window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || false;
-	}
-	if (!('hidden' in document)) {
-		document.hidden = document.webkitHidden || document.msHidden || document.mozHidden || false;
-	}
-	if (!('visibilityState' in document)) {
-		document.visibilityState = document.webkitVisibilityState || document.msVisibilityState || document.mozVisibilityState || 'visible';
-	}
-	if (!('fullscreenElement' in document)) {
-		document.fullscreenElement = document.mozFullScreenElement || document.webkitFullscreenElement || false;
-	}
-	if (!('requestAnimationFrame' in window)) {
-		window.requestAnimationFrame = window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || false;
-	}
-	if (!('exitFullscreen' in Document.prototype)) {
-		Document.prototype.exitFullscreen = document.mozCancelFullScreen || document.webkitCancelFullScreen || document.msCancelFullScreen || false;
-	}
-	if (!('requestFullscreen' in HTMLElement.prototype)) {
-		HTMLElement.prototype.requestFullScreen = HTMLElement.prototype.mozRequestFullScreen || HTMLElement.prototype.webkitRequestFullScreen || false;
-	}
+
+if (!('Notification' in window)) {
+	window.Notification = window.notifications || window.webkitNotifications || window.oNotifications || window.msNotifications || false;
+}
+if (!('indexedDB' in window)) {
+	window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || false;
+}
+if (!('hidden' in document)) {
+	document.hidden = document.webkitHidden || document.msHidden || document.mozHidden || false;
+}
+if (!('visibilityState' in document)) {
+	document.visibilityState = document.webkitVisibilityState || document.msVisibilityState || document.mozVisibilityState || 'visible';
+}
+if (!('fullscreenElement' in document)) {
+	document.fullscreenElement = document.mozFullScreenElement || document.webkitFullscreenElement || false;
+}
+if (!('requestAnimationFrame' in window)) {
+	window.requestAnimationFrame = window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || false;
+}
+if (!('exitFullscreen' in Document.prototype)) {
+	Document.prototype.exitFullscreen = document.mozCancelFullScreen || document.webkitCancelFullScreen || document.msCancelFullScreen || false;
+}
+if (!('requestFullscreen' in HTMLElement.prototype)) {
+	HTMLElement.prototype.requestFullScreen = HTMLElement.prototype.mozRequestFullScreen || HTMLElement.prototype.webkitRequestFullScreen || false;
 }
