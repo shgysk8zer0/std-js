595504a305e607f066435def7fc2d953780bf14e
Chris Zuber <shgysk8zer0@gmail.com>
2018-04-20 12:33:48 -0700

    Enhance CC validation with auto-validation for autocomplete attribute
    Now exports multiple functions.
    - Basic validation (now handles 13 | 16 digits)
    - A function for custom validation as an event listener
    - A function to set as listener automatically by autocomplete attr

--- credit-card-validator.js
+++ credit-card-validator.js
@@ -1,14 +1,15 @@
 /**
  * Validates a 16 digit credit card number
  *
- * @param  string ccnum   Any 16 digit number as a string
+ * @param  string ccnum   Any 13 | 16 digit number as a string
  * @return boolean        Whether or not it Validates
  */
-export default function(ccnum) {
-	if (ccnum.length !== 16 || ! /^\d+$/.test(ccnum)) {
+export function isValidCC(ccnum) {
+	ccnum = ccnum.replace('-', '').replace(' ', '');
+	if ((ccnum.length !== 16 && ccnum.length !== 13) || ! /^\d+$/.test(ccnum)) {
 		return false;
 	}
-	let sums = ccnum.split('').map(num => parseInt(num)).reduce((nums, num, i) => {
+	const sums = ccnum.split('').map(num => parseInt(num)).reduce((nums, num, i) => {
 		if (i % 2  === 0) {
 			nums[0] += num;
 			if (num >= 5) {
@@ -20,4 +21,24 @@ export default function(ccnum) {
 		return nums;
 	}, [0, 0, 0]);
 	return (sums[0] * 2 + sums[1] + sums[2]) % 10 === 0;
+}
+
+export function validateCCInput(event) {
+	if (HTMLInputElement.prototype.hasOwnProperty('setCustomValidity')) {
+		if (isValidCC(event.target.value)) {
+			event.target.setCustomValidity('');
+		} else {
+			event.target.setCustomValidity('Please enter a valid credit card number');
+		}
+	} else {
+		event.target.removeEventListener(event.type, validateCCInput);
+	}
+}
+
+export function setCCValidator() {
+	if (HTMLInputElement.prototype.hasOwnProperty('setCustomValidity')) {
+		Array.from(document.querySelectorAll('[autocomplete="cc-number"]')).forEach(input => {
+			input.addEventListener('change', validateCCInput);
+		});
+	}
 }
