16e7225fd51a77c2d99327f1ffd00d6a39e0a7c5
Chris Zuber <shgysk8zer0@gmail.com>
2018-11-17 16:32:18 -0800

    Add class for GravatarURLs

---
+++ GravatarURL.js
@@ -0,0 +1,68 @@
+import md5 from './std-js/md5.js';
+
+export default class GravatarURL extends URL {
+	constructor(hash, {
+		size = 80,
+		fallback = 'mp',
+		force = false,
+		rating = null,
+	} = {}) {
+		super(hash, 'https://secure.gravatar.com/avatar/');
+
+		if (typeof size === 'number' && ! Number.isNaN(size)) {
+			this.size = size;
+		}
+
+		if (typeof fallback === 'string') {
+			this.fallback = fallback;
+		}
+
+		if (typeof force === 'boolean') {
+			this.force = force;
+		}
+
+		if (typeof rating === 'string') {
+			this.rating = rating;
+		}
+	}
+
+	static getHash(email) {
+		return md5(email);
+	}
+
+	set size(size) {
+		this.searchParams.set('s', size);
+	}
+
+	get size() {
+		return parseInt(this.searchParams.get('s')) || 80;
+	}
+
+	set fallback(fallback) {
+		this.searchParams.set('d', fallback);
+	}
+
+	get fallback() {
+		return this.searchParams.get('d');
+	}
+
+	set force(force) {
+		if (force) {
+			this.searchParams.set('f', 'y');
+		} else {
+			this.searchParams.delete('f');
+		}
+	}
+
+	get force() {
+		return this.searchParams.has('f');
+	}
+
+	set rating(rating) {
+		this.searchParams.set('r', rating);
+	}
+
+	get rating() {
+		return this.searchParams.set('r');
+	}
+}
