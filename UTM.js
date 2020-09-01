d09ceb0656d622784018c7598ee1bb93a72cf291
Chris Zuber <shgysk8zer0@gmail.com>
2020-09-01 14:06:59 -0700

    Add UTM validator and generator class

---
+++ UTM.js
@@ -0,0 +1,109 @@
+export default class UTM extends URL {
+	constructor(url = location.href, {
+		campaign = null,
+		content  = null,
+		medium   = null,
+		source   = null,
+		term     = null,
+	} = {}) {
+		super(url);
+
+		if (typeof campaign === 'string') {
+			this.campaign = campaign;
+		}
+
+		if (typeof content === 'string') {
+			this.content = content;
+		}
+
+		if (typeof medium === 'string') {
+			this.medium = medium;
+		}
+
+		if (typeof source === 'string') {
+			this.source = source;
+		}
+
+		if (typeof term === 'string') {
+			this.term = term;
+		}
+	}
+
+	toJSON() {
+		const  { campaign, source, medium, content, term } = this;
+		return { campaign, source, medium, content, term };
+	}
+
+	get campaign() {
+		return this.searchParams.get('utm_campaign');
+	}
+
+	set campaign(val) {
+		if (typeof val === 'string') {
+			this.searchParams.set('utm_campaign', val.toLowerCase());
+		} else {
+			this.searchParams.delete('utm_campaign');
+		}
+	}
+
+	get content() {
+		return this.searchParams.get('utm_content');
+	}
+
+	set content(val) {
+		if (typeof val === 'string') {
+			this.searchParams.set('utm_content', val.toLowerCase());
+		} else {
+			this.searchParams.delete('utm_content');
+		}
+	}
+
+	get medium() {
+		return this.searchParams.get('utm_medium');
+	}
+
+	set medium(val) {
+		if (typeof val === 'string') {
+			this.searchParams.set('utm_medium', val.toLowerCase());
+		} else {
+			this.searchParams.delete('utm_medium');
+		}
+	}
+
+	get source() {
+		return this.searchParams.get('utm_source');
+	}
+
+	set source(val) {
+		if (typeof val === 'string') {
+			this.searchParams.set('utm_source', val.toLowerCase());
+		} else {
+			this.searchParams.delete('utm_source');
+		}
+	}
+
+	get term() {
+		return this.searchParams.get('utm_term');
+	}
+
+	set term(val) {
+		if (typeof val === 'string') {
+			this.searchParams.set('utm_term', val.toLowerCase());
+		} else {
+			this.searchParams.delete('utm_term');
+		}
+	}
+
+	get valid() {
+		return ['utm_source', 'utm_medium', 'utm_campaign'].every(param => this.searchParams.has(param));
+	}
+
+	clear() {
+		this.searchParams.delete('utm_source');
+		this.searchParams.delete('utm_medium');
+		this.searchParams.delete('utm_campaign');
+		this.searchParams.delete('utm_content');
+		this.searchParams.delete('utm_term');
+		return this;
+	}
+}
