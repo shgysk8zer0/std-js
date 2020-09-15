117eb714c99d2146956bc0c7297473a6bbe700c6
Chris Zuber <shgysk8zer0@gmail.com>
2020-09-14 17:32:50 -0700

    Add set of callbacks for GA click events

--- google-analytics.js
+++ google-analytics.js
@@ -1,3 +1,4 @@
+/* global ga */
 import { loadScript } from './loader.js';
 
 window.dataLayer = window.dataLayer || [];
@@ -21,3 +22,48 @@ export async function importGa(id, params = {}) {
 	return gtag;
 }
 
+export function externalHandler() {
+	ga('send', {
+		hitType: 'event',
+		eventCategory: 'outbound',
+		eventAction: 'click',
+		eventLabel: this.href,
+		transport: 'beacon',
+	});
+}
+
+export function telHandler() {
+	ga('send', {
+		hitType: 'event',
+		eventCategory: 'call',
+		eventLabel: this.href.replace('tel:', '').trim(),
+		transport: 'beacon',
+	});
+}
+
+export function mailtoHandler() {
+	ga('send', {
+		hitType: 'event',
+		eventCategory: 'email',
+		eventLabel: this.href.replace('mailto:', '').trim(),
+		transport: 'beacon',
+	});
+}
+
+export function geoHandler() {
+	ga('send', {
+		hitType: 'event',
+		eventCategory: 'geo',
+		eventLabel: this.href.replace('geo:', '').trim(),
+		transport: 'beacon',
+	});
+}
+
+export function genericHandler() {
+	ga('send', {
+		hitType: this.dataset.hitType || 'event',
+		eventCategory: this.dataset.eventCategory || 'unknown',
+		eventLabel: this.dataset.eventLabel || 'unknown',
+		transport: this.dataset.transport || 'beacon',
+	});
+}
