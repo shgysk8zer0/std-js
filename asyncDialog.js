00c663eff3f8efcdc314405ad0b6743e679dcc27
Chris Zuber <shgysk8zer0@gmail.com>
2020-11-27 14:22:45 -0800

    Update asyncDialog.js
    Update button rules and styles

--- asyncDialog.js
+++ asyncDialog.js
@@ -1,11 +1,28 @@
+function css(el, rules) {
+	Object.entries(rules).forEach(([prop, value]) => el.style.setProperty(prop, value));
+}
+
+const btnStyles = {
+	'max-width': '180px',
+};
+
+const btnContainerStyles = {
+	'justify-content': 'center',
+	'gap': '16px',
+};
+
 export async function alert(text) {
 	return await new Promise(resolve => {
 		const dialog = document.createElement('dialog');
 		const msg = document.createElement('div');
 		const close = document.createElement('button');
+		const btns = document.createElement('div');
+		css(btns, btnContainerStyles);
+		btns.classList.add('flex', 'row', 'no-wrap');
+		close.classList.add('btn', 'btn-primary', 'grow-1');
+		css(close, btnStyles);
 
 		dialog.classList.add('clearfix', 'animation-speed-normal', 'animation-ease-in', 'fadeInUp');
-		close.classList.add('float-right');
 		msg.textContent = text;
 		close.textContent = 'Ok';
 
@@ -14,7 +31,8 @@ export async function alert(text) {
 			resolve();
 		});
 		close.addEventListener('click', event => event.target.closest('dialog[open]').close());
-		dialog.append(msg, close);
+		btns.append(close);
+		dialog.append(msg, document.createElement('br'), btns);
 		document.body.append(dialog);
 		dialog.showModal();
 	});
@@ -26,19 +44,21 @@ export async function confirm(text) {
 		const msg = document.createElement('div');
 		const close = document.createElement('button');
 		const ok = document.createElement('button');
+		const btns = document.createElement('div');
+		css(btns, btnContainerStyles);
 
 		close.type = 'button';
 		ok.type = 'button';
-
-		ok.classList.add('btn', 'btn-accept');
-		close.classList.add('btn', 'btn-reject');
+		ok.classList.add('btn', 'btn-accept', 'grow-1');
+		close.classList.add('btn', 'btn-reject', 'grow-1');
 		dialog.classList.add('animation-speed-normal', 'animation-ease-in', 'fadeInUp');
+		btns. classList.add('flex', 'row', 'no-wrap');
+		css(ok, btnStyles);
+		css(close, btnStyles);
 
 		msg.textContent = text;
 		close.textContent = 'Cancel';
 		ok.textContent = 'Ok';
-
-		ok.style.setProperty('margin-right', '0.4em');
 
 		dialog.addEventListener('close', event => {
 			event.target.remove();
@@ -53,7 +73,9 @@ export async function confirm(text) {
 			event.target.closest('dialog[open]').close('confirm');
 		});
 
-		dialog.append(msg, ok, close);
+		btns.append(ok, close);
+
+		dialog.append(msg, document.createElement('br'), btns);
 		document.body.append(dialog);
 		dialog.showModal();
 	});
@@ -67,6 +89,8 @@ export async function prompt(text, defaultValue = '') {
 		const ok = document.createElement('button');
 		const form = document.createElement('form');
 		const input = document.createElement('input');
+		const btns = document.createElement('div');
+		css(btns, btnContainerStyles);
 
 		close.type = 'button';
 		ok.type = 'submit';
@@ -74,16 +98,27 @@ export async function prompt(text, defaultValue = '') {
 		input.placeholder = defaultValue;
 		input.value = defaultValue;
 		input.name = 'result';
+		css(input, {
+			width: '100%',
+			background: 'transparent',
+			'border-width': '0 0 2px 0',
+			'border-color': 'currentColor',
+			color: 'inherit',
+			margin: '8px',
+			padding: '4px 8px',
+		});
 
-		ok.classList.add('btn', 'btn-accept');
-		close.classList.add('btn', 'btn-reject');
+		css(ok, btnStyles);
+		css(close, btnStyles);
+
+		ok.classList.add('btn', 'btn-primary', 'grow-1', 'shrink-0');
+		close.classList.add('btn', 'btn-reject', 'grow-1');
 		dialog.classList.add('animation-speed-normal', 'animation-ease-in', 'fadeInUp');
+		btns.classList.add('flex', 'row', 'no-wrap', 'center');
 
 		msg.textContent = text;
 		close.textContent = 'Cancel';
 		ok.textContent = 'Ok';
-		input.style.setProperty('margin-bottom', '12px');
-		ok.style.setProperty('margin-right', '0.4em');
 
 		dialog.addEventListener('close', event => event.target.remove());
 
@@ -99,7 +134,9 @@ export async function prompt(text, defaultValue = '') {
 			resolve(data.get('result'));
 		});
 
-		form.append(input, document.createElement('br'), ok, close);
+		btns.append(ok, close);
+
+		form.append(input, document.createElement('br'), btns);
 
 		dialog.append(msg, form);
 		document.body.append(dialog);
