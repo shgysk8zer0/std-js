f117cbcfa75258560f6fb3d8617010231aa1a724
Chris Zuber <shgysk8zer0@gmail.com>
2020-12-15 15:32:56 -0800

    Support more SVG attributes

--- functions.js
+++ functions.js
@@ -183,9 +183,20 @@ export function changeTagName(target, tag = 'div', { replace = true, is = null }
 	return el;
 }
 
-export function createSVG({ fill = null, height = null, width = null } = {}) {
+export function createSVG({ fill = null, height = null, width = null, label = null,
+	role = 'img', hidden = false, classes = [] } = {}) {
 	const xmlns = 'http://www.w3.org/2000/svg';
 	const svg = document.createElementNS(xmlns, 'svg');
+
+	svg.setAttribute('role', role);
+
+	if (typeof label === 'string') {
+		svg.setAttribute('aria-label', label);
+	}
+
+	if (hidden === true) {
+		svg.setAttribute('aria-hidden', 'true');
+	}
 
 	if (typeof height === 'number') {
 		svg.setAttribute('height', height);
@@ -199,18 +210,28 @@ export function createSVG({ fill = null, height = null, width = null } = {}) {
 		svg.setAttribute('fill', fill);
 	}
 
+	if (Array.isArray(classes) && classes.length !== 0) {
+		svg.classList.add(...classes);
+	}
+
 	return svg;
 }
 
-export function useSVG(sprite, { src = '/img/icons.svg', fill = null, height = null, width = null } = {}) {
-	const url = new URL(src, document.baseURI);
+export function useSVG(sprite, { src = '/img/icons.svg', fill = null, height = null,
+	width = null, classes = [], label = null } = {}) {
 	const xmlns = 'http://www.w3.org/2000/svg';
 	const xlink = 'http://www.w3.org/1999/xlink';
-	const svg = createSVG({ fill, height, width });
+	const svg = createSVG({ fill, height, width, classes, label, hidden: true });
 	const use = document.createElementNS(xmlns, 'use');
 
-	url.hash = `#${sprite}`;
-	use.setAttributeNS(xlink, 'xlink:href', url.href);
+	if (typeof src === 'string') {
+		const url = new URL(src, document.baseURI);
+		url.hash = `#${sprite}`;
+		use.setAttributeNS(xlink, 'xlink:href', url.href);
+	} else {
+		use.setAttributeNS(xlink, 'xlink:href', `#${sprite}`);
+	}
+
 	svg.append(use);
 
 	return svg;
