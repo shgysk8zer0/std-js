107f152a3b8942d08fe36f4a09e640a9df233d83
Chris Zuber <shgysk8zer0@gmail.com>
2020-12-24 09:27:44 -0800

    Update Color.js
    Now supports opacity and optional hex opacity formats when parsing.

--- Color.js
+++ Color.js
@@ -1,66 +1,117 @@
-export default class Color extends EventTarget {
-	constructor({r = 0, g = 0, b = 0} = {}) {
-		super();
-		this.red = r;
-		this.green = g;
-		this.blue = b;
+function validRGB(val) {
+	return Math.max(0, Math.min(parseInt(val), 255));
+}
+
+export default class Color {
+	constructor({ red = 0, green = 0, blue = 0, opacity = 1 } = {}) {
+		this.red = red;
+		this.green = green;
+		this.blue = blue;
+		this.opacity = opacity;
 	}
 
 	get red() {
-		return this._r;
+		return validRGB(this._r);
 	}
 
 	set red(val) {
-		this._r = parseInt(val);
-		this.dispatchEvent(new CustomEvent('change', {detail: this}));
+		this._r = validRGB(val);
 	}
 
 	get green() {
-		return this._g;
+		return validRGB(this._g);
 	}
 
 	set green(val) {
-		this._g = parseInt(val);
-		this.dispatchEvent(new CustomEvent('change', {detail: this}));
+		this._g = validRGB(val);
 	}
 
 	get blue() {
-		return this._b;
+		return validRGB(this._b);
 	}
 
 	set blue(val) {
-		this._b = parseInt(val);
-		this.dispatchEvent(new CustomEvent('change', {detail: this}));
+		this._b = validRGB(val);
+	}
+
+	get opacity() {
+		return Math.max(0, Math.min(this._o, 1));
+	}
+
+	set opacity(val) {
+		this._o = Math.max(0, Math.min(parseFloat(val), 1));
 	}
 
 	toString() {
-		const {red, green, blue} = this;
-		return `#${red.toString(16).padStart(2, '0')}${green.toString(16).padStart(2, '0')}${blue.toString(16).padStart(2, '0')}`;
+		if (this.opacity === 1) {
+			return this.toHexString();
+		} else {
+			return this.toRGBAString();
+		}
 	}
 
 	toJSON() {
-		return {r: this.red, g: this.greeb, b: this.green};
+		return this.toString();
+	}
+
+	toHexString() {
+		const { red, green, blue } = this;
+		return `#${red.toString(16).padStart(2, '0')}${green.toString(16).padStart(2, '0')}${blue.toString(16).padStart(2, '0')}`;
+	}
+
+	toRGBString() {
+		const { red, green, blue } = this;
+		return `rgb(${red}, ${green}, ${blue})`;
+	}
+
+	toRGBAString() {
+		const { red, green, blue, opacity } = this;
+		return `rgb(${red}, ${green}, ${blue}, ${opacity})`;
 	}
 
 	static parse(hex) {
-		if (hex.startsWith('#')) {
+		if (typeof hex !== 'string') {
+			throw new Error('Cannot parse a color from a non-string code');
+		} else if (hex.startsWith('#')) {
 			hex = hex.substring(1);
 		}
 
 		if (hex.length === 6) {
 			return new Color({
-				r: parseInt(hex.slice(0, 2), 16),
-				g: parseInt(hex.slice(2, 4), 16),
-				b: parseInt(hex.slice(4, 6), 16),
+				red: parseInt(hex.slice(0, 2), 16),
+				green: parseInt(hex.slice(2, 4), 16),
+				blue: parseInt(hex.slice(4, 6), 16),
 			});
 		} else if (hex.length === 3) {
 			return new Color({
-				r: parseInt(hex.slice(0, 1).repeat(2), 16),
-				g: parseInt(hex.slice(1, 2).repeat(2), 16),
-				b: parseInt(hex.slice(2, 3).repeat(2), 16),
+				red: parseInt(hex.slice(0, 1).repeat(2), 16),
+				green: parseInt(hex.slice(1, 2).repeat(2), 16),
+				blue: parseInt(hex.slice(2, 3).repeat(2), 16),
+			});
+		} else if (hex.length === 4) {
+			return new Color({
+				red: parseInt(hex.slice(0, 1).repeat(2), 16),
+				green: parseInt(hex.slice(1, 2).repeat(2), 16),
+				blue: parseInt(hex.slice(2, 3).repeat(2), 16),
+				opacity: parseInt(hex.slice(3, 4).repeat(2), 16) / 255,
+			});
+		} else if (hex.length === 8) {
+			return new Color({
+				red: parseInt(hex.slice(0, 2), 16),
+				green: parseInt(hex.slice(2, 4), 16),
+				blue: parseInt(hex.slice(4, 6), 16),
+				opacity: parseInt(hex.slice(6, 8), 16) / 255,
 			});
 		} else {
-			throw new Error('Hex colors must be 3 or 6 characters long');
+			throw new Error('Not a valid Hex color code');
 		}
+	}
+
+	static rgb(red, green, blue) {
+		return new Color({ red, green, blue });
+	}
+
+	static rgba(red, green, blue, opacity) {
+		return new Color({ red, green, blue, opacity });
 	}
 }
