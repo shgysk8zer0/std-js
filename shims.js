97be1a960d77abf30e4f49b2629e181a6e3816ba
Chris Zuber <shgysk8zer0@gmail.com>
2021-02-08 06:58:45 -0800

    Make `navigator.setAppBadge()` work via title
    Set to match errors thrown when not it or n is negative.

--- shims.js
+++ shims.js
@@ -47,7 +47,23 @@ if (! ( window.cancelIdleCallback instanceof Function)) {
 }
 
 if (! (navigator.setAppBadge instanceof Function)) {
-	navigator.setAppBadge = () => Promise.reject('Not Supported');
+	navigator.setAppBadge = async (n) => {
+		if (! Number.isInteger(n)) {
+			throw new TypeError('Failed to execute \'setAppBadge\' on \'Navigator\': Value is not of type \'unsigned long long\'');
+		} else if (n < 0) {
+			throw new TypeError('Failed to execute \'setAppBadge\' on \'Navigator\': Value is outside the \'unsigned long long\' value range.');
+		} else if (n === 0) {
+			if (document.title.startsWith('(')) {
+				document.title = document.title.replace(/^\((\*|\d{1,2})\)\s/, '');
+			}
+		} else if (n < 100) {
+			await navigator.clearAppBadge();
+			document.title = `(${n}) ${document.title}`;
+		} else {
+			await navigator.clearAppBadge();
+			document.title = `(*) ${document.title}`;
+		}
+	};
 }
 
 if (! (navigator.clearAppBadge instanceof Function)) {
