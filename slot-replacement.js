a919b524b8e48427cc938e5084699d123f121d14
Chris Zuber <shgysk8zer0@gmail.com>
2021-04-30 10:49:25 -0700

    Update slot-replacement.js

--- slot-replacement.js
+++ slot-replacement.js
@@ -12,9 +12,20 @@ function changeHandler() {
 	update(this);
 }
 
-export function slotReplacementHandler(shadow) {
-	shadow.querySelectorAll('slot[name]').forEach(slot => {
-		update(slot);
-		slot.addEventListener('slotchange', changeHandler);
-	});
+export function slotReplacementHandler(shadow, ...slots) {
+	if (slots.length === 0) {
+		shadow.querySelectorAll('slot[name]').forEach(slot => {
+			update(slot);
+			slot.addEventListener('slotchange', changeHandler);
+		});
+	} else {
+		slots.forEach(name => {
+			const slot = shadow.querySelector(`slot[name="${name}"]`);
+
+			if (slot instanceof HTMLSlotElement) {
+				update(slot);
+				slot.addEventListener('slotchange', changeHandler);
+			}
+		});
+	}
 }
