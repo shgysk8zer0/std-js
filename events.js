022809f4ed125bc5afe6f20739f6317edf8c488e
Chris Zuber <shgysk8zer0@gmail.com>
2021-04-30 17:30:08 -0700

    Move more functions from `functions.js` -> `events.js`

--- events.js
+++ events.js
@@ -1,4 +1,4 @@
-import { query } from './dom.js';
+import { query, when } from './dom.js';
 import { signalAborted } from './abort.js';
 
 function getEventFeatures() {
@@ -75,4 +75,58 @@ export function removeListener(targets, events, callback, { capture, once, passi
 	targets.forEach(target => {
 		events.forEach(event => target.removeEventListener(event, callback, { capture, once, passive, signal }));
 	});
+}
+
+/**
+ * Control the execution rate of callbacks, i.e. for listeners
+ * https://davidwalsh.name/function-debounce
+ *
+ * @param  Callable callback    The callback
+ * @param  int      [ms]        Number of milliseconds, defaulting to 1/60th a second
+ * @param  bool     [immediate] Trigger function immediately instead of after wait ms
+ * @return function             Rate limited function
+ */
+export function debounce(func, wait = 17, immediate = false) {
+	let timeout;
+
+	return function(...args) {
+		const later = () => {
+			timeout = null;
+
+			if (! immediate) {
+				func.apply(this, args);
+			}
+		};
+
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+
+		if (immediate && ! timeout) {
+			func.apply(this, args);
+		}
+	};
+}
+
+export async function whenOnline() {
+	if (navigator.onLine === false) {
+		await when(window, 'online');
+	}
+}
+
+export async function whenOffline() {
+	if (navigator.onLine === true) {
+		await when(window, 'offline');
+	}
+}
+
+export async function whenVisible() {
+	if (document.visibilityState === 'hidden') {
+		await when(document, 'visibilitychange');
+	}
+}
+
+export async function whenHidden() {
+	if (document.visibilityState === 'visible') {
+		await when(document, 'visibilitychange');
+	}
 }
