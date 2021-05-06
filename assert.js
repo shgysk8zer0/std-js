01a9ae724d81045807ef0d988d43c8c3902a6f9c
Chris Zuber <shgysk8zer0@gmail.com>
2021-05-05 17:13:27 -0700

    Implement `deepEquals()` and add param to not throw

--- assert.js
+++ assert.js
@@ -4,65 +4,104 @@ export function isAsync(what) {
 	return what instanceof Function && what.constructor.name === 'AsyncFunction';
 }
 
-const DEFAULT_EXCEPTION = new AssertionError('Assertion failed');
+const DEFAULT_EXCEPTION = 'Assertion failed';
 
-const LITERALS = ['string', 'number', 'boolean'];
+const LITERALS = ['string', 'number', 'boolean', 'undefined', 'symbol'];
 
-export function assert(test, { exception = DEFAULT_EXCEPTION } = {}) {
+function getError(exception = DEFAULT_EXCEPTION) {
+	return exception instanceof Error ? exception : new AssertionError(exception);
+}
+
+export function assert(test, { exception = DEFAULT_EXCEPTION, throws = true } = {}) {
 	if (test !== true) {
-		throw exception instanceof Error ? exception : new AssertionError(exception);
+		if (throws === true) {
+			throw getError(exception);
+		} else {
+			return false;
+		}
 	} else {
 		return true;
 	}
 }
 
-export function equals(a, b, { exception } = {}) {
-	return assert(a === b, { exception });
-}
-
-export function isString(test, { minLength: min = 0, maxLength: max, exception } = {}) {
-	if (Number.isInteger(max)) {
-		return isString(test, { exception }) && isInteger(test.length, { min, max, exception });
+export function equals(a, b, { exception, throws } = {}) {
+	if (Number.isNaN(a)) {
+		return isNaN(b, { exception, throws });
 	} else {
-		return assert(typeof test === 'string', { exception });
+		return assert(a === b, { exception, throws });
 	}
 }
 
-export function matches(str, exp, { exception } = {}) {
-	return assert(exp.test(str), { exception });
-}
-
-export function doesNotMatch(str, exp, { exception } = {}) {
-	return assert(! exp.test(str), { exception });
-}
-
-export function isNumber(test, { exception, max = Number.MAX_SAFE_INTEGER, min = Number.MIN_SAFE_INTEGER } = {}) {
-	return assert(typeof test === 'number' && ! Number.isNaN(test) && test <= max && test >= min, { exception });
-}
-
-export function isInteger(test, { min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGER, exception } = {}) {
-	return assert(Number.isInteger(test) && test >= min && test <= max, { exception });
-}
-
-export function isNaN(test, { exception } = {}) {
-	return assert(Number.isNaN(test), { exception });
-}
-
-export function isArray(test, { minLength: min = 0, maxLength: max, exception } = {}) {
-	if (Number.isInteger(max)) {
-		return isArray(test, { exception }) && isInteger(test.length, { min, max, exception });
+export function deepEquals(a, b, { exception, throws } = {}) {
+	if (a === b) {
+		return true;
+	} else if (typeof a !== typeof b) {
+		if (throws) {
+			throw getError(exception);
+		} else {
+			return false;
+		}
+	} else if (LITERALS.includes(typeof a)) {
+		return equals(a, b, { exception, throws });
+	} else if (Array.isArray(a)) {
+		return assert(a.length === b.length && (a.every(val => b.includes(val)
+			|| b.some(valb => deepEquals(val, valb, { throws: false }))
+		)), { exception, throws });
+	} else if (a !== null) {
+		return deepEquals(Object.keys(a), Object.keys(b), { exception, throws })
+			&& Object.entries(a).every(([key, value]) => deepEquals(value, b[key], { exception, throws }));
 	} else {
-		return assert(Array.isArray(test, { exception }));
+		return equals(b, null, { exception, throws })
 	}
 }
 
-export function instanceOf(test, expected, { exception } = {}) {
-	return assert(test instanceof expected, { exception });
+export function isString(test, { minLength: min = 0, maxLength: max, exception, throws } = {}) {
+	if (Number.isInteger(max)) {
+		return isString(test, { exception, throws }) && isInteger(test.length, { min, max, exception, throws });
+	} else {
+		return assert(typeof test === 'string', { exception, throws });
+	}
 }
 
-export function throws(callback, { exception, thisArg = null, args = [] } = {}) {
+export function includes(test, search, { exception, throws } = {}) {
+	return assert(test.includes(search), { exception, throws });
+}
+
+export function matches(str, exp, { exception, throws } = {}) {
+	return assert(exp.test(str), { exception, throws });
+}
+
+export function doesNotMatch(str, exp, { exception, throws } = {}) {
+	return assert(! exp.test(str), { exception, throws });
+}
+
+export function isNumber(test, { exception, max = Number.MAX_SAFE_INTEGER, min = Number.MIN_SAFE_INTEGER, throws } = {}) {
+	return assert(typeof test === 'number' && ! Number.isNaN(test) && test <= max && test >= min, { exception, throws });
+}
+
+export function isInteger(test, { min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGER, exception, throws } = {}) {
+	return assert(Number.isInteger(test) && test >= min && test <= max, { exception, throws });
+}
+
+export function isNaN(test, { exception, throws } = {}) {
+	return assert(Number.isNaN(test), { exception, throws });
+}
+
+export function isArray(test, { minLength: min, maxLength: max, exception, throws } = {}) {
+	if (Number.isInteger(max) || Number.isInteger(min)) {
+		return isArray(test, { exception, throws }) && isInteger(test.length, { min, max, exception, throws });
+	} else {
+		return assert(Array.isArray(test, { exception, throws }));
+	}
+}
+
+export function instanceOf(test, expected, { exception, throws } = {}) {
+	return assert(test instanceof expected, { exception, throws });
+}
+
+export function throws(callback, { exception, thisArg = null, args = [], throws } = {}) {
 	if (isAsync(callback)) {
-		return rejects(callback(...args), { exception });
+		return rejects(callback(...args), { exception, throws });
 	} else {
 		let thrown = false;
 
@@ -72,13 +111,13 @@ export function throws(callback, { exception, thisArg = null, args = [] } = {}) 
 			thrown = true;
 		}
 
-		return assert(thrown, { exception });
+		return assert(thrown, { exception, throws });
 	}
 }
 
-export function doesNotThrow(callback, { exception, thisArg = null, args = [] } = {}) {
+export function doesNotThrow(callback, { exception, thisArg = null, args = [], throws } = {}) {
 	if (isAsync(callback)) {
-		return rejects(callbac(...args));
+		return resolves(callback(...args), { exception, throws });
 	} else {
 		let thrown = false;
 
@@ -88,18 +127,18 @@ export function doesNotThrow(callback, { exception, thisArg = null, args = [] } 
 			thrown = true;
 		}
 
-		return assert(! thrown, { exception });
+		return assert(! thrown, { exception, throws });
 	}
 }
 
-export async function resolves(promise, { exception } = {}) {
+export async function resolves(promise, { exception, throws } = {}) {
 	let thrown = false;
 	await promise.catch(() => thrown = true);
-	return assert(! thrown, { exception });
+	return assert(! thrown, { exception, throws });
 }
 
-export async function rejects(promise, { exception } = {}) {
+export async function rejects(promise, { exception, throws } = {}) {
 	let thrown = false;
 	await promise.catch(() => thrown = true);
-	return assert(thrown, { exception });
+	return assert(thrown, { exception, throws });
 }
