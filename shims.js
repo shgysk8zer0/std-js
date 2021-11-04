5aacc50d3f2e997f3df69ea1a23e5683abc212a9
Chris Zuber <shgysk8zer0@gmail.com>
2021-11-04 12:30:51 -0700

    Update handling of sharing
    Since Safari is an (unfortunately) common browser that does not support `<button is="share-button">`, provide a simple alternative using `data-share-*` attributes.
    
    Also add `navigator.canShare()` to `shims.js`

--- shims.js
+++ shims.js
@@ -187,6 +187,20 @@ if (! (Object.fromEntries instanceof Function)) {
 	};
 }
 
+if (! (navigator.canShare instanceof Function)) {
+	navigator.canShare = function({ title, text, url, files } = {}) {
+		if (! (navigator.share instanceof Function)) {
+			return false;
+		} else if (Array.isArray(files) && files.length !== 0) {
+			return false;
+		} else if ([title, text, url].every(arg => typeof arg === 'undefined')) {
+			return true;
+		} else {
+			return [title, text, url].some(arg => typeof arg === 'string' && arg.length !== 0);
+		}
+	}
+}
+
 if (! (navigator.setAppBadge instanceof Function)) {
 	navigator.setAppBadge = async (n) => {
 		if (! Number.isInteger(n)) {
