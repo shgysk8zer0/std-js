6530e4818508987a7c81715571842988780567bd
Chris Zuber <shgysk8zer0@gmail.com>
2021-12-15 16:21:31 -0800

    Update handling of lock data
    `name` is now only passed in the contructor

--- AsyncLock.js
+++ AsyncLock.js
@@ -2,16 +2,13 @@ import { getDeferred, sleep } from './promises.js';
 const locks = new WeakMap();
 
 export class AsyncLock extends EventTarget {
-	constructor(name, { mode = 'exclusive', steal = false, ifAvailable = false, signal } = {}) {
+	constructor(name) {
 		super();
-
-		if (typeof name === 'string') {
-			this.lock(name, { mode, steal, ifAvailable, signal });
-		}
+		locks.set(this, { name });
 	}
 
 	get locked() {
-		return locks.has(this);
+		return 'lock' in locks.get(this);
 	}
 
 	get mode() {
@@ -23,11 +20,7 @@ export class AsyncLock extends EventTarget {
 	}
 
 	get name() {
-		if (this.locked) {
-			return locks.get(this).lock.name;
-		} else {
-			return null;
-		}
+		return locks.get(this).name;
 	}
 
 	get supported() {
@@ -62,20 +55,21 @@ export class AsyncLock extends EventTarget {
 		});
 	}
 
-	async lock(name, { mode = 'exclusive', ifAvailable = false, steal = false, signal, expires } = {}) {
+	async lock({ mode = 'exclusive', ifAvailable = false, steal = false, signal, expires } = {}) {
 		if (this.locked) {
 			throw new DOMException('Already locked');
 		} else if (! this.supported) {
 			throw new DOMException('WebLocks not supported');
 		} else {
 			const deferred = getDeferred({ signal });
+			const name = this.name;
 
 			navigator.locks.request(name, { mode, ifAvailable, steal, signal }, async lock => {
 				if (! lock) {
 					deferred.reject(new DOMException('Error acquiring lock'));
 				} else {
 					const { resolve, reject, promise } = getDeferred({ signal });
-					locks.set(this, { resolve, reject, promise, lock });
+					locks.set(this, { name, resolve, reject, promise, lock });
 					deferred.resolve(lock);
 					this.dispatchEvent(new CustomEvent('locked', { detail: lock }));
 
@@ -84,12 +78,12 @@ export class AsyncLock extends EventTarget {
 					}
 
 					return await promise.then(detail => {
-						locks.delete(this);
+						locks.set(this, { name });
 						resolve(detail);
 						this.dispatchEvent(new CustomEvent('unlocked', { detail }));
 						return detail;
 					}).catch(error => {
-						locks.delete(this);
+						locks.set(this, { name });
 						reject(error);
 						this.dispatchEvent(new CustomEvent('error', { detail: error }));
 						this.dispatchEvent(new CustomEvent('unlocked', { detail: { error }}));
@@ -105,8 +99,8 @@ export class AsyncLock extends EventTarget {
 		if (! this.locked) {
 			throw new DOMException('Attempting to unlock a lock which is not locked');
 		} else {
-			const { resolve, reject, lock } = locks.get(this);
-			locks.delete(this);
+			const { resolve, reject, lock, name } = locks.get(this);
+			locks.set(this, { name });
 
 			if (reason instanceof Error) {
 				reject(reason);
