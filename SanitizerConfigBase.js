b5d9a51995b5683f8e87ec75b1c7aa2f45dbbeab
Chris Zuber <shgysk8zer0@gmail.com>
2022-01-11 15:33:32 -0800

    Update list of blocked/dropped tags

--- SanitizerConfigBase.js
+++ SanitizerConfigBase.js
@@ -4,9 +4,9 @@ export const allowAttributes = undefined;
 export const allowComments = false;
 export const allowCustomElements = false;
 export const allowElements = undefined;
-export const blockElements = ['iframe', 'frame', 'object', 'embed', 'param'];
+export const blockElements = ['frame'];
 export const dropAttributes = Object.fromEntries(events.map(event => [event, ['*']]));
-export const dropElements = ['script', 'link', 'title'];
+export const dropElements = ['script', 'link', 'title', 'noscript', 'head', 'body', 'object', 'embed', 'param', 'iframe'];
 
 export const SanitizerConfig = {
 	allowAttributes, allowComments, allowElements, allowCustomElements,
