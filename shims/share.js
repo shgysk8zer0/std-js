1e9c04f0e38ef0c5bf428a52246fe9e1f3d80939
Chris Zuber <shgysk8zer0@gmail.com>
2022-01-12 18:04:19 -0800

    Break shims down into separate modules
    Most are moved to own files in `/shims/`

---
+++ shims/share.js
@@ -0,0 +1,13 @@
+if (! (navigator.canShare instanceof Function)) {
+	navigator.canShare = function({ title, text, url, files } = {}) {
+		if (! (navigator.share instanceof Function)) {
+			return false;
+		} else if (Array.isArray(files) && files.length !== 0) {
+			return false;
+		} else if ([title, text, url].every(arg => typeof arg === 'undefined')) {
+			return true;
+		} else {
+			return [title, text, url].some(arg => typeof arg === 'string' && arg.length !== 0);
+		}
+	};
+}
