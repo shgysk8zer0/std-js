5a2d9d4a16574523c93354b64cddd56ef84321d1
Chris Zuber <shgysk8zer0@gmail.com>
2022-01-12 18:05:27 -0800

    Delete all.js

--- shims/all.js
+++
@@ -1,7 +0,0 @@
-import './abort.js';
-import { trustPolicies as defaultPolicies } from './trustedTypes.js';
-import { trustPolicies as sanitizerPolicies } from './sanitizer.js';
-import './cookieStore.js';
-import './locks.js';
-
-export const trustPolicies = [...defaultPolicies, ...sanitizerPolicies];
