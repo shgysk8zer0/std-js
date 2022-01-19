83d2e72ed031c8316ce785451717fe7609a314e7
Chris Zuber <shgysk8zer0@gmail.com>
2022-01-18 16:24:24 -0800

    Update polyfill for `TextEncoder.encodeInto()`
    Just apply from the prototype of the polyfill class...

--- shims/textEncoder.js
+++ shims/textEncoder.js
@@ -10,20 +10,7 @@ if (! ('TextDecoder' in globalThis)) {
 }
 
 if (! (globalThis.TextEncoder.prototype.encodeInto instanceof Function)) {
-	globalThis.TextEncoder.prototype.encodeInto = function(str, ui8arr) {
-		if (! (ui8arr instanceof Uint8Array)) {
-			throw new TypeError('TextEncoder.encodeInto: Argument 2 does not implement interface Uint8Array.');
-		} else {
-			const encoded = this.encode(str);
-			const len = Math.min(ui8arr.length, encoded.length);
-
-			if (encoded.length > ui8arr.length) {
-				ui8arr.set(encoded.slice(0, len));
-			} else {
-				ui8arr.set(encoded);
-			}
-
-			return { read: len, written: len };
-		}
+	globalThis.TextEncoder.prototype.encodeInto = function(...args) {
+		return TextEncoder.prototype.encodeInto.apply(this, args)
 	};
 }
