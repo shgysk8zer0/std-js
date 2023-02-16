8c4c5440f362e9cc879a0f39a2eaeec13f444f28
Chris Zuber <shgysk8zer0@gmail.com>
2023-02-16 08:29:21 -0800

    Add `Iterator.range` polyfill

--- shims/number.js
+++ shims/number.js
@@ -1,18 +1,19 @@
 if (! (Number.range instanceof Function)) {
 	/**
 	 * @see https://github.com/tc39/proposal-Number.range
+	 * @deprecated - changed to `Iterator.range`
 	 */
 	Number.range = function* range(start, end, step = 1) {
-		if (! [start, end, step].every(n => ! Number.isNaN(n) && (Number.isInteger(n) || n === Infinity))) {
-			throw new TypeError('`start`, `end`, and `step` must all be integers.');
-		} else if (step === 0 || start === end || (start > end && step > 1) || (start < end && step < 1)) {
-			throw new RangeError(`Invalid range: {"start": ${start}, "end": ${end}, "step": ${step}}`);
+		if (! [start, end, step].every(n => typeof n === 'number' && ! Number.isNaN(n))) {
+			throw new TypeError('`start`, `end`, and `step` must all be numbers.');
+		} else if (step === 0) {
+			throw new RangeError(`Invalid step: ${step}`);
 		} else if (step > 0) {
 			for (let n = start; n <= end; n += step) {
 				yield n;
 			}
 		} else {
-			for (let n = start;n >= end; n += step) {
+			for (let n = start; n >= end; n += step) {
 				yield n;
 			}
 		}
