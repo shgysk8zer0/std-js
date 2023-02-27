4bbe7365682ba84ea218d8cefca63f30863b7ef0
Chris Zuber <shgysk8zer0@gmail.com>
2023-02-27 11:58:38 -0800

    Add collection of trust policies

--- test/index.js
+++ test/index.js
@@ -8,8 +8,9 @@ import { sleep } from '../promises.js';
 import { toggleClass, on, replaceClass, data, attr, ready } from '../dom.js';
 import { init } from '../data-handlers.js';
 import { description, keywords, robots, thumbnail } from '../meta.js';
-import { SanitizerConfig as sanitizerConfig } from '../SanitizerConfig.js';
-import { createPolicy } from '../trust.js';
+import { getDefaultPolicy } from '../trust-policies.js';
+
+getDefaultPolicy();
 
 scheduler.postTask(() =>  Promise.allSettled([
 	import('https://cdn.kernvalley.us/components/leaflet/map.min.js'),
@@ -20,63 +21,11 @@ scheduler.postTask(() =>  Promise.allSettled([
 	import('https://cdn.kernvalley.us/components/youtube/player.js'),
 	import('https://cdn.kernvalley.us/components/spotify/player.js'),
 	import('https://cdn.kernvalley.us/components/bacon-ipsum.js'),
+	import('https://cdn.kernvalley.us/components/github/gist.js'),
+	import('https://cdn.kernvalley.us/components/codepen-embed.js'),
+	import('https://cdn.kernvalley.us/components/weather/current.js'),
+	import('https://cdn.kernvalley.us/components/weather/forecast.js'),
 ]), { priority: 'background' }).then(console.log);
-
-const sanitizer = new Sanitizer({
-	...sanitizerConfig,
-	allowCustomElements: true,
-	allowElements: [
-		...sanitizerConfig.allowElements, 'krv-ad', 'github-user', 'leaflet-map',
-		'leaflet-marker', 'krv-events', 'weather-current', 'weather-forecast',
-		'spotify-player', 'youtube-player', 'github-repo', 'bacon-ipsum',
-	],
-	allowAttributes: {
-		...sanitizerConfig.allowAttributes,
-		'url': ['krv-ad', 'spotify-player'],
-		'uri': ['spotify-player'],
-		'video': ['youtube-player'],
-		'theme': ['krv-ad', 'krv-events', 'weather-current', 'weather-forecast'],
-		'appid': ['weather-current', 'weather-forecast'],
-		'paras': ['bacon-ipsum'],
-		'postalcode': ['weather-current', 'weather-forecast'],
-		'source': ['krv-ad'],
-		'medium': ['krv-ad'],
-		'content': ['krv-ad'],
-		'campaign': ['krv-ad'],
-		'layout': ['krv-ad'],
-		'user': ['github-user', 'github-repo'],
-		'repo': ['github-repo'],
-		'bio': ['github-user'],
-		'loading': ['krv-ad', 'github-user'],
-		'zoomcontrol': ['leaflet-map'],
-		'allowfullscreen': ['leaflet-map'],
-		'allowlocate': ['leaflet-map'],
-		'router': ['leaflet-map'],
-		'zoom': ['leflet-map'],
-		'minzoom': ['leaflet-map', 'leaflet-marker'],
-		'maxzoom': ['leaflet-map', 'leaflet-marker'],
-		'center': ['leaflet-map'],
-		'latitude': ['leaflet-marker'],
-		'longitude': ['leaflet-marker'],
-		'open': ['leaflet-marker'],
-
-	},
-});
-globalThis.sanitizer = sanitizer;
-
-createPolicy('default', {
-	createHTML: input => sanitizer.sanitizeFor('div', input).innerHTML,
-	createScriptURL: input => {
-		const url = new URL(input, document.baseURI);
-
-		if ([location.origin, 'https://cdn.kernvalley.us'].includes(url.origin)) {
-			return input;
-		} else {
-			return '';
-		}
-	},
-	createScript: () => globalThis.trustedTypes.emptyScript.toString(),
-});
 
 keywords(['javascript', 'ecmascript', 'es6', 'modules', 'library']);
 description('This is a JavaScript library testing page');
