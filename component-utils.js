1e540d97cc58e8ba64e1a9729425d70dc702fe17
Chris Zuber <shgysk8zer0@gmail.com>
2023-04-06 10:53:59 -0700

    Create module providing easy use of other modules

---
+++ component-utils.js
@@ -0,0 +1,7 @@
+export { registerCustomElement } from './custom-elements.js';
+export { loadStylesheet, getTemplateLoader } from './loader.js';
+export { getURLResolver } from './utility.js';
+export { whenIntersecting } from './intersect.js';
+export { meta } from '../../export.meta.js';
+export { createPolicy } from './trust.js';
+export * from './attrs.js';
